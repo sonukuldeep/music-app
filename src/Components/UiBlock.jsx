f924df1cec12f167378f4f2bdaed6c5acd843eee
sonukuldeep <sonukuldeep@outlook.com>
2022-09-05 13:11:32 +0530

    removed audioDB api as its no longer free and added paxabay links

--- src/Components/UiBlock.jsx
+++ src/Components/UiBlock.jsx
@@ -1,13 +1,27 @@
 import './UB.css'
 ;import albumArt from "../images/album-art.png";
-import playControls from "../images/playControls.jpg";
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import NoteContext from "../Context/Notes/NoteContext";
+import { Link } from 'react-router-dom';
 
 
 const UiBlock = () => {
     const data = useContext(NoteContext);
-    // console.log(data);
+    const [musicScr, setMusicScr] = useState("")
+    const play = index => {
+        // const audioEl = document.querySelector('audio')
+        // audioEl.play();
+        setMusicScr(data[index].src)
+        console.log(musicScr);
+
+    }
+
+    useEffect(() => {
+        const audioEl = document.querySelector('audio')
+        // audioEl.pause();
+        audioEl.play();
+    }, [musicScr])
+    
   return (
     <>
       <section >
@@ -17,11 +31,17 @@ const UiBlock = () => {
                             <img src={albumArt} alt='album art'></img>
                         </div>
                         <div className="item">
-                            {/* <img src={playlist} alt='playlist'></img> */}
-                            <p> this is the user name {data.name}</p>
+                            <ul>
+                            <li><span>Song:- <Link to="#" onClick={()=>{play(0)}}>{data[0].title} by: {data[0].artist}</Link></span></li>
+                            <li><span>Song:- <Link to="#" onClick={()=>{play(1)}}>{data[1].title} by: {data[1].artist}</Link></span></li>
+                            <li><span>Song:- <Link to="#" onClick={()=>{play(2)}}>{data[2].title} by: {data[2].artist}</Link></span></li>
+                            <li><span>Song:- <Link to="#" onClick={()=>{play(3)}}>{data[3].title} by: {data[3].artist}</Link></span></li>
+                            </ul>
                         </div>
-                        <div className="item">
-                            <img src={playControls} alt='play control'></img>
+                        <div className="item audio-element">
+                        <audio controls> 
+                            <source src={musicScr} type="audio/mpeg"/>
+                            </audio>
                         </div>
                     </div>
 
--- src/Context/Notes/NoteState.jsx
+++ src/Context/Notes/NoteState.jsx
@@ -1,15 +1,37 @@
 import NoteContext from "./NoteContext";
-import React from "react";
+import React, {useState} from "react";
 
 const NoteState = (props) => {
-    const state = {
-        'name': 'Sonu',
-        'class': '2'
-    }
+  const [state, setState] = useState(
+ [{
+    'title': 'Tuesday',
+    'artist': 'Amaksi',
+    'image': '',
+    'audioSrc': 'https://cdn.pixabay.com/download/audio/2022/08/25/audio_4f3b0a816e.mp3?filename=tuesday-glitch-soft-hip-hop-118327.mp3'
+  },
+  {
+    'title': 'The Blackest Bouquet',
+    'artist': 'Leonell Cassio',
+    'image': '',
+    'audioSrc': 'https://cdn.pixabay.com/download/audio/2022/08/31/audio_419263fc12.mp3?filename=leonell-cassio-the-blackest-bouquet-118766.mp3'
+  },
+  {
+    'title': 'Goldn',
+    'artist': 'Prazkhal',
+    'image': '',
+    'audioSrc': 'https://cdn.pixabay.com/download/audio/2022/08/04/audio_2dde668d05.mp3?filename=goldn-116392.mp3'
+  },
+  {
+    'title': 'Milk Shake',
+    'artist': 'Coma-media',
+    'image': '',
+    'audioSrc': 'https://cdn.pixabay.com/download/audio/2022/08/04/audio_2dde668d05.mp3?filename=goldn-116392.mp3'
+  }
+  ])
 
   return (
-    <NoteContext.Provider value={state}>
-        {props.children}
+    <NoteContext.Provider value={state} stateFun={setState}>
+      {props.children}
     </NoteContext.Provider>
   )
 }
