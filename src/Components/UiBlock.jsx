e8721ab6c693ff37615fa3ea5199f2cb5ff71d55
sonukuldeep <sonukuldeep@outlook.com>
2022-09-06 23:56:37 +0530

    mute/unmute button and time elapsed and total time counter added

--- src/Components/UiBlock.jsx
+++ src/Components/UiBlock.jsx
@@ -13,19 +13,49 @@ const UiBlock = () => {
     const [currentSongIndex, setCurrentSongIndex] = useState(0);
     const [nextSongIndex, setNextSongIndex] = useState(1);
     const [previousSong, setPreviousSong] = useState(songData.length - 1)
-    function getRanSongIndex(){
+
+    function getRanSongIndex() {
         setCurrentSongIndex(Math.floor(Math.random() * songData.length))
     }
-    
+
+    function muteUnmute() {
+        audioE1.current.muted = !audioE1.current.muted
+    }
+    const [currentTrackLength, setCurrentTrackLength] = useState(0)
+    const [currentDuration, setCurrentDuration] = useState(0)
+    const timeFormating = (timeInSeconds) => { return (new Date(timeInSeconds * 1000).toISOString().substring((timeInSeconds > 3600) ? 11 : (timeInSeconds > 60) ? 14 : 15, 19)) }
+
+    setInterval(() => {
+
+        try {
+            let duration = Math.ceil(audioE1.current.currentTime)
+            setCurrentDuration(timeFormating(duration))
+            // console.log(duration)
+        }
+        catch (err) {
+            console.log('no music playing')
+        }
+    }, 1000);
+
+
+
     useEffect(() => {
+        audioE1.current.play()
+        
         setNextSongIndex(() => {
-            audioE1.current.play();
             return ((currentSongIndex + 1) > (songData.length - 1) ? 0 : (currentSongIndex + 1));
         }
         )
-        setPreviousSong(()=>{
+        setPreviousSong(() => {
             return (currentSongIndex === 0) ? (songData.length - 1) : (currentSongIndex - 1)
         })
+
+        setTimeout(() => {
+            let trackLength = Math.floor(audioE1.current.duration)
+            setCurrentTrackLength(timeFormating(trackLength))
+            
+        }, 200);
+
     }, [currentSongIndex, songData.length])
 
 
@@ -33,6 +63,8 @@ const UiBlock = () => {
         isPlaying ? audioE1.current.play() : audioE1.current.pause()
 
     }, [isPlaying])
+
+
 
 
     return (
@@ -47,7 +79,8 @@ const UiBlock = () => {
                             <ul>
                                 <li>current song title: {songData[currentSongIndex].title}</li>
                                 <li>by: {songData[currentSongIndex].artist}</li>
-                                
+                                <li>by: {songData[currentSongIndex].artist}</li>
+
                             </ul>
                         </div>
                         <div className="item audio-element">
@@ -55,12 +88,19 @@ const UiBlock = () => {
                                 controls
                                 src={songData[currentSongIndex].audioSrc}
                                 ref={audioE1}
+                                muted={true}
                             >
                             </audio>
                             <div>
-                            <button className='playerControls' onClick={() => { setCurrentSongIndex(previousSong) }}>Previous</button>
-                            <button className='playerControls' onClick={() => { setCurrentSongIndex(nextSongIndex) }}>Next</button>
-                            <button className='playerControls' onClick={getRanSongIndex}>Shuffle</button>
+                                <button className='playerControls' onClick={() => { setCurrentSongIndex(previousSong) }}>Previous</button>
+                                <button className='playerControls' onClick={() => { setCurrentSongIndex(nextSongIndex) }}>Next</button>
+                                <button className='playerControls' onClick={getRanSongIndex}>Shuffle</button>
+                                <button className='playerControls' onClick={muteUnmute}>Mute/Unmute</button>
+                                {/* <button className='playerControls' onClick={show}>showTime</button> */}
+                                <div>
+                                    <span>{currentDuration}</span>
+                                    <span> / {currentTrackLength}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
